7c881d8cf4e700aca9885175ba163c40fb81b70c
Gaurangjodh <jodhgaurang@gmail.com>
2025-01-25 14:12:45 +0530

    Displaying the books in the table format

--- script.js
+++ script.js
@@ -12,11 +12,26 @@ function addBookToLibrary(title, author, pages, readStatus){
     myLibrary.push(newBook);
 }
 
+const table = document.querySelector("#bookTable");
 function displayBooks(){
-    for (const book in myLibrary) {
-        if (Object.prototype.hasOwnProperty.call(myLibrary, book)) {
-            const element = myLibrary[book];
-            
-        }
+    for (let index = 0; index < myLibrary.length; index++) {
+        const book = myLibrary[index];
+        let row = table.insertRow(index+1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        let cell5 = row.insertCell(4);
+
+        cell1.innerText = `${index+1}`;
+        cell2.innerText = `${book.title}`;
+        cell3.innerText = `${book.author}`;
+        cell4.innerText = `${book.pages}`;
+        cell5.innerText = `${book.readStatus}`;
     }
 }
+
+addBookToLibrary("Lil nigga", "Kanye", 3, "read");
+addBookToLibrary("Lil ", "ye", 30, "not read");
+addBookToLibrary("igga", "west", 123, "read");
+displayBooks();
